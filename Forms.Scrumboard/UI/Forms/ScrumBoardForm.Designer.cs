9e3bf97ebc160d16ec39b4eea96604c5f08876ca
Willem van Rijbroek <Willem.van.Rijbroek@ctac.nl>
2014-11-21 10:31:23 +0100

    Corrected background color

--- Forms.Scrumboard/UI/Forms/ScrumBoardForm.Designer.cs
+++ Forms.Scrumboard/UI/Forms/ScrumBoardForm.Designer.cs
@@ -163,7 +163,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(555, 335);
             this.ContextMenuStrip = this.contextMenuStrip1;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
